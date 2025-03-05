6edc646b079cc5ccb324a82a6d7666344701317c
MedvedEE <kahurpoiss@gmail.com>
2025-03-05 08:52:49 +0200

    Spring boot setup

---
+++ lennuplaan/src/main/java/com/medvedee/lennuplaan/LennuplaanApplication.java
@@ -0,0 +1,19 @@
+package com.medvedee.lennuplaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+@SpringBootApplication
+public class LennuplaanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LennuplaanApplication.class, args);
+    }
+
+}
+
+
---
+++ lennuplaan/src/test/java/com/medvedee/lennuplaan/LennuplaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medvedee.lennuplaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennuplaanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
